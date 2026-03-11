2e05e65aac01ab1650dbcb2f7793802c1993462d
Vasu <vasugoel1729@gmail.com>
2026-03-11 22:07:32 +0530

    Initial commit with my folder files

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import Userform
+from .models import User
+# Create your views here.
+def user_view(request):
+    if request.method=="POST":
+        form=Userform(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form=Userform()
+    users = User.objects.all()
+    return render(request,'user.html',{'form':form,'users':users})
