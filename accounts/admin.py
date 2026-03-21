1027a88b60f337260f08d266a504388fe8b73401
Vasu <vasugoel1729@gmail.com>
2026-03-21 23:52:06 +0530

    CRUD FOR PRODUCT

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User,Category
+from .models import User,Category,Product
 admin.site.register(User)
 admin.site.register(Category)
+admin.site.register(Product)
+
 # Register your models here.
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import user_view,category_view ,update_category,read_category,delete_category
+from .views import *
 urlpatterns=[
     path("form/",user_view),
     path("category_new/",category_view,name='category_create'),
     path('category_update/',update_category),
     path('read/',read_category),
     path('delete/',delete_category,name="delete"),
+    path('products/', product, name='product_page'),
+    path('products/edit/<int:pk>/', edit_product, name='edit_product'),
+    path('products/delete/<int:pk>/', delete_product, name='delete_product'),
 ]
